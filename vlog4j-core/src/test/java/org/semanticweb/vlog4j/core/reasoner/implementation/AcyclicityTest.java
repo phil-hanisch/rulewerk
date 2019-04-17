479ea4474b1f49db5a000ed7d5b59703540543df
David Carral <david.carral@tu-dresden.de>
2019-04-17 20:24:08 +0200

    Testing MFC

--- vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/reasoner/implementation/AcyclicityTest.java
+++ vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/reasoner/implementation/AcyclicityTest.java
@@ -3,15 +3,21 @@ package org.semanticweb.vlog4j.core.reasoner.implementation;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeConjunction;
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeConstant;
+import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makePositiveConjunction;
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makePositiveLiteral;
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeRule;
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeVariable;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+import org.semanticweb.vlog4j.core.model.api.Conjunction;
 import org.semanticweb.vlog4j.core.model.api.Constant;
+import org.semanticweb.vlog4j.core.model.api.Literal;
 import org.semanticweb.vlog4j.core.model.api.PositiveLiteral;
 import org.semanticweb.vlog4j.core.model.api.Predicate;
 import org.semanticweb.vlog4j.core.model.api.Rule;
@@ -54,17 +60,21 @@ public class AcyclicityTest {
 	final Predicate edbV = new PredicateImpl("edbV", 2);
 	final Predicate idbV = new PredicateImpl("idbV", 2);
 
-	final Rule mappingRuleC = makeRule(makePositiveLiteral(idbC, x), makePositiveLiteral(edbC, x));
-	final Rule mappingRuleD = makeRule(makePositiveLiteral(idbD, x), makePositiveLiteral(edbD, x));
-	final Rule mappingRuleR = makeRule(makePositiveLiteral(idbR, x, y), makePositiveLiteral(edbR, x, y));
-	final Rule mappingRuleS = makeRule(makePositiveLiteral(idbS, x, y), makePositiveLiteral(edbS, x, y));
-	final Rule mappingRuleV = makeRule(makePositiveLiteral(idbV, x, y), makePositiveLiteral(edbV, x, y));
+	final Rule mappingC = makeRule(makePositiveLiteral(idbC, x), makePositiveLiteral(edbC, x));
+	final Rule mappingD = makeRule(makePositiveLiteral(idbD, x), makePositiveLiteral(edbD, x));
+	final Rule mappingE = makeRule(makePositiveLiteral(idbE, x), makePositiveLiteral(edbE, x));
+	final Rule mappingR = makeRule(makePositiveLiteral(idbR, x, y), makePositiveLiteral(edbR, x, y));
+	final Rule mappingS = makeRule(makePositiveLiteral(idbS, x, y), makePositiveLiteral(edbS, x, y));
+	final Rule mappingV = makeRule(makePositiveLiteral(idbV, x, y), makePositiveLiteral(edbV, x, y));
+	final List<Rule> mappings = Arrays.asList(mappingC, mappingD, mappingE, mappingR, mappingS, mappingV);
 
-	final PositiveLiteral factInstEDBC = makePositiveLiteral(edbC, c);
-	final PositiveLiteral factInstEDBD = makePositiveLiteral(edbD, c);
-	final PositiveLiteral factInstEDBR = makePositiveLiteral(edbR, c, d);
-	final PositiveLiteral factInstEDBS = makePositiveLiteral(edbS, c, d);
-	final PositiveLiteral factInstEDBV = makePositiveLiteral(edbV, c, d);
+	final PositiveLiteral instC = makePositiveLiteral(edbC, c);
+	final PositiveLiteral instD = makePositiveLiteral(edbD, c);
+	final PositiveLiteral instE = makePositiveLiteral(edbD, c);
+	final PositiveLiteral instR = makePositiveLiteral(edbR, c, d);
+	final PositiveLiteral instS = makePositiveLiteral(edbS, c, d);
+	final PositiveLiteral instV = makePositiveLiteral(edbV, c, d);
+	final List<PositiveLiteral> instEDBs = Arrays.asList(instC, instD, instE, instR, instS, instV);
 
 	@Test
 	public void testAcyclicityJA() throws ReasonerStateException, EdbIdbSeparationException,
@@ -77,8 +87,9 @@ public class AcyclicityTest {
 		final Rule rule2 = makeRule(makePositiveLiteral(idbV, y, z), makePositiveLiteral(idbS, x, y));
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
-			reasoner.addRules(mappingRuleR, mappingRuleS, mappingRuleV, rule1, rule2);
-			reasoner.addFacts(factInstEDBR, factInstEDBS, factInstEDBV);
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2);
 			reasoner.load();
 			assertTrue(reasoner.isJA());
 			assertTrue(reasoner.isRJA());
@@ -104,8 +115,9 @@ public class AcyclicityTest {
 				makePositiveLiteral(idbS, x, y));
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
-			reasoner.addRules(mappingRuleC, mappingRuleD, mappingRuleR, mappingRuleS, rule1, rule2);
-			reasoner.addFacts(factInstEDBC, factInstEDBD, factInstEDBR, factInstEDBS);
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2);
 
 			reasoner.load();
 			assertFalse(reasoner.isJA());
@@ -131,8 +143,9 @@ public class AcyclicityTest {
 		// idbR(?y, ?x) :- idbR(?x, ?y)
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
-			reasoner.addRules(mappingRuleC, mappingRuleD, mappingRuleR, mappingRuleS, rule1, rule2);
-			reasoner.addFacts(factInstEDBC, factInstEDBD, factInstEDBR, factInstEDBS);
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2);
 
 			reasoner.load();
 			assertFalse(reasoner.isJA());
@@ -151,13 +164,18 @@ public class AcyclicityTest {
 			IncompatiblePredicateArityException, IOException, NotStartedException {
 		// JA, RJA, not MFA, RMFA, not MFC, not RMFC, and CyclicityResult.ACYCLIC
 
-		final Rule rule1 = makeRule(makePositiveLiteral(idbR, x, y), makePositiveLiteral(idbC, x));
-		final Rule rule2 = makeRule(makePositiveLiteral(idbC, z), makePositiveLiteral(idbR, v, z));
-		final Rule rule3 = makeRule(makePositiveLiteral(idbR, w, u), makePositiveLiteral(idbR, u, w));
+		// idbR(?x, ?y), idbC(?y) :- idbC(?x)
+		Conjunction<PositiveLiteral> head1 = makePositiveConjunction(makePositiveLiteral(idbR, x, y),
+				makePositiveLiteral(idbC, y));
+		Conjunction<Literal> body1 = makeConjunction(makePositiveLiteral(idbC, x));
+		final Rule rule1 = makeRule(head1, body1);
+		// idbR(?y, ?x) :- idbR(?x, ?y)
+		final Rule rule2 = makeRule(makePositiveLiteral(idbR, y, x), makePositiveLiteral(idbR, x, y));
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
-			reasoner.addRules(mappingRuleC, mappingRuleD, mappingRuleR, mappingRuleS, rule1, rule2, rule3);
-			reasoner.addFacts(factInstEDBC, factInstEDBD, factInstEDBR, factInstEDBS);
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2);
 
 			reasoner.load();
 			assertFalse(reasoner.isJA());
@@ -171,51 +189,85 @@ public class AcyclicityTest {
 		}
 	}
 
-	// @Test
-	// public void testCyclicity() throws ReasonerStateException,
-	// EdbIdbSeparationException,
-	// IncompatiblePredicateArityException, IOException, NotStartedException {
-	// final Literal epXY = makePositiveLiteral("ep", x, y);
-	// final PositiveLiteral ipXY = makePositiveLiteral("ip", x, y);
-	// final PositiveLiteral ipYZ = makePositiveLiteral("ip", y, z);
-	//
-	// // ip(?x, ?y) :- ep(?x, ?y)
-	// final Rule rule1 = makeRule(ipXY, epXY);
-	// // ip(?y, ?z) :- ip(?x, ?y)
-	// final Rule rule2 = makeRule(ipYZ, ipXY);
-	// final PositiveLiteral fact = makePositiveLiteral("ep", c, d);
-	//
-	// try (final Reasoner reasoner = Reasoner.getInstance()) {
-	// reasoner.addRules(rule1);
-	// reasoner.addRules(rule2);
-	// reasoner.addFacts(fact);
-	//
-	// reasoner.load();
-	// assertEquals(CyclicityResult.CYCLIC, reasoner.checkForCycles());
-	// }
-	// }
-	//
-	// @Test
-	// public void testAcyclicity() throws ReasonerStateException,
-	// EdbIdbSeparationException,
-	// IncompatiblePredicateArityException, IOException, NotStartedException {
-	// final Literal epXY = makePositiveLiteral("ep", x, y);
-	// final PositiveLiteral ip1XY = makePositiveLiteral("ip1", x, y);
-	// final PositiveLiteral ip2YZ = makePositiveLiteral("ip2", y, z);
-	//
-	// // ip1(?x, ?y) :- ep(?x, ?y)
-	// final Rule rule1 = makeRule(ip1XY, epXY);
-	// // ip2(?y, ?z) :- ip1(?x, ?y)
-	// final Rule rule2 = makeRule(ip2YZ, ip1XY);
-	// final PositiveLiteral fact = makePositiveLiteral("ep", c, d);
-	//
-	// try (final Reasoner reasoner = Reasoner.getInstance()) {
-	// reasoner.addRules(rule1);
-	// reasoner.addRules(rule2);
-	// reasoner.addFacts(fact);
-	//
-	// reasoner.load();
-	// assertEquals(CyclicityResult.ACYCLIC, reasoner.checkForCycles());
-	// }
-	// }
+	@Test
+	public void testAcyclicityRMFA() throws ReasonerStateException, EdbIdbSeparationException,
+			IncompatiblePredicateArityException, IOException, NotStartedException {
+		// not JA, not RJA, not MFA, RMFA, MFC, not RMFC, and
+		// CyclicityResult.ACYCLIC
+
+		// idbR(?x, ?y), idbD(?y) :- idbC(?x)
+		Conjunction<PositiveLiteral> head1 = makePositiveConjunction(makePositiveLiteral(idbR, x, y),
+				makePositiveLiteral(idbD, y));
+		Conjunction<Literal> body1 = makeConjunction(makePositiveLiteral(idbC, x));
+		final Rule rule1 = makeRule(head1, body1);
+		// idbS(?x, ?y), idbE(?y) :- idbD(?x)
+		Conjunction<PositiveLiteral> head2 = makePositiveConjunction(makePositiveLiteral(idbS, x, y),
+				makePositiveLiteral(idbE, y));
+		Conjunction<Literal> body2 = makeConjunction(makePositiveLiteral(idbD, x));
+		final Rule rule2 = makeRule(head2, body2);
+		// idbV(?x, ?y), idbC(?y) :- idbD(?x)
+		Conjunction<PositiveLiteral> head3 = makePositiveConjunction(makePositiveLiteral(idbV, x, y),
+				makePositiveLiteral(idbC, y));
+		Conjunction<Literal> body3 = makeConjunction(makePositiveLiteral(idbE, x));
+		final Rule rule3 = makeRule(head3, body3);
+		// idbR(?z, ?x) :- idbS(?x, ?y), idbV(?y, ?z)
+		Conjunction<PositiveLiteral> head4 = makePositiveConjunction(makePositiveLiteral(idbR, z, x));
+		Conjunction<Literal> body4 = makeConjunction(makePositiveLiteral(idbS, x, y), makePositiveLiteral(idbV, y, z));
+		final Rule rule4 = makeRule(head4, body4);
+
+		try (final Reasoner reasoner = Reasoner.getInstance()) {
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2, rule3, rule4);
+
+			reasoner.load();
+			assertFalse(reasoner.isJA());
+			assertFalse(reasoner.isRJA());
+			assertFalse(reasoner.isMFA());
+			assertTrue(reasoner.isRMFA());
+			assertFalse(reasoner.isMFC());
+			// Uncomment the following line when the RMFC check is implemented
+			// assertFalse(reasoner.isRMFC());
+			assertEquals(CyclicityResult.ACYCLIC, reasoner.checkForCycles());
+		}
+	}
+
+	@Test
+	public void testAcyclicityMFC() throws ReasonerStateException, EdbIdbSeparationException,
+			IncompatiblePredicateArityException, IOException, NotStartedException {
+		// not JA, not RJA, not MFA, not RMFA, MFC, RMFC, and
+		// CyclicityResult.CYCLIC
+
+		// idbR(?x, ?y), idbD(?y) :- idbC(?x)
+		Conjunction<PositiveLiteral> head1 = makePositiveConjunction(makePositiveLiteral(idbR, x, y),
+				makePositiveLiteral(idbD, y));
+		Conjunction<Literal> body1 = makeConjunction(makePositiveLiteral(idbC, x));
+		final Rule rule1 = makeRule(head1, body1);
+		// idbS(?x, ?y), idbE(?y) :- idbD(?x)
+		Conjunction<PositiveLiteral> head2 = makePositiveConjunction(makePositiveLiteral(idbS, x, y),
+				makePositiveLiteral(idbE, y));
+		Conjunction<Literal> body2 = makeConjunction(makePositiveLiteral(idbD, x));
+		final Rule rule2 = makeRule(head2, body2);
+		// idbV(?x, ?y), idbC(?y) :- idbD(?x)
+		Conjunction<PositiveLiteral> head3 = makePositiveConjunction(makePositiveLiteral(idbV, x, y),
+				makePositiveLiteral(idbC, y));
+		Conjunction<Literal> body3 = makeConjunction(makePositiveLiteral(idbE, x));
+		final Rule rule3 = makeRule(head3, body3);
+
+		try (final Reasoner reasoner = Reasoner.getInstance()) {
+			reasoner.addRules(mappings);
+			reasoner.addFacts(instEDBs);
+			reasoner.addRules(rule1, rule2, rule3);
+
+			reasoner.load();
+			assertFalse(reasoner.isJA());
+			assertFalse(reasoner.isRJA());
+			assertFalse(reasoner.isMFA());
+			assertFalse(reasoner.isRMFA());
+			assertTrue(reasoner.isMFC());
+			// Uncomment the following line when the RMFC check is implemented
+			// assertTrue(reasoner.isRMFC());
+			assertEquals(CyclicityResult.ACYCLIC, reasoner.checkForCycles());
+		}
+	}
 }
