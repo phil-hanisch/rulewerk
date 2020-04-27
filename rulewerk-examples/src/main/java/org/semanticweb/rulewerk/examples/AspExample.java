c09cea48fc4c2d335b73cd59530ca5a9068a8d28
Philipp Hanisch <philipp.hanisch1@tu-dresden.de>
2020-04-27 16:10:30 +0200

    compute facts for helper literals

--- rulewerk-examples/src/main/java/org/semanticweb/rulewerk/examples/AspExample.java
+++ rulewerk-examples/src/main/java/org/semanticweb/rulewerk/examples/AspExample.java
@@ -24,17 +24,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import org.semanticweb.rulewerk.core.reasoner.KnowledgeBase;
 import org.semanticweb.rulewerk.core.reasoner.LogLevel;
 import org.semanticweb.rulewerk.core.reasoner.Reasoner;
+import org.semanticweb.rulewerk.core.reasoner.QueryResultIterator;
 import org.semanticweb.rulewerk.core.reasoner.implementation.VLogReasoner;
 import org.semanticweb.rulewerk.parser.ParsingException;
 import org.semanticweb.rulewerk.parser.RuleParser;
 import org.semanticweb.rulewerk.core.model.api.Rule;
 import org.semanticweb.rulewerk.core.model.api.Variable;
 import org.semanticweb.rulewerk.core.model.api.Literal;
+import org.semanticweb.rulewerk.core.model.api.PositiveLiteral;
 import org.semanticweb.rulewerk.core.model.api.Conjunction;
 import org.semanticweb.rulewerk.core.model.implementation.RuleImpl;
 import org.semanticweb.rulewerk.core.model.implementation.ConjunctionImpl;
@@ -77,46 +80,57 @@ public class AspExample {
 		KnowledgeBase kbModified = new KnowledgeBase();
 		kbModified.addStatements(kb.getFacts());
 		int ruleIdx = 0;
+		List<PositiveLiteral> helperLiterals = new ArrayList();
 		for (Rule rule : kb.getRules()) {
 			String bodyUniversalVariableNames = rule.getBody().getUniversalVariables()
 								   .reduce("", (partialNames, variable) -> partialNames.equals("") 
 								   		? variable.toString()
 								   		: partialNames + "," + variable.toString(), String::concat);
 		    String helperString = "rule" + ruleIdx + "(" + bodyUniversalVariableNames + ")";
-	   		Literal helperLiteral = RuleParser.parsePositiveLiteral(helperString);
-			List<Literal> helperLiterals = Arrays.asList(helperLiteral);
-			Conjunction helperConjunction = new ConjunctionImpl(helperLiterals);
+	   		PositiveLiteral helperLiteral = RuleParser.parsePositiveLiteral(helperString);
+			Conjunction helperConjunction = new ConjunctionImpl(Arrays.asList(helperLiteral));
 
 			kbModified.addStatement(new RuleImpl(helperConjunction, rule.getBody()));
 			kbModified.addStatement(new RuleImpl(rule.getHead(), helperConjunction));
 
+			helperLiterals.add(helperLiteral);
 			ruleIdx++;
 		}
-		
+
 		System.out.println("Modified rules used in this example:");
 		kbModified.getRules().forEach(System.out::println);
 		System.out.println("");
 
-		// Execute reasoning
-
-		// Get grounded rules
-
-		try (Reasoner reasoner = new VLogReasoner(kb)) {
+		/* Execute reasoning */
+		try (Reasoner reasoner = new VLogReasoner(kbModified)) {
 			reasoner.setLogFile(ExamplesUtils.OUTPUT_FOLDER + "vlog.log");
 			reasoner.setLogLevel(LogLevel.DEBUG);
 
 			/* Initialise reasoner and compute inferences */
 			reasoner.reason();
 
-			/* Execute some queries */
-			final List<String> queries = Arrays.asList("colour(?X)", "coloured(?X, ?C)", "vertex(?X)");
-			System.out.println("\nNumber of inferred tuples for selected query atoms:");
-			for (final String queryString : queries) {
-				ExamplesUtils.printOutQueryAnswers(RuleParser.parsePositiveLiteral(queryString), reasoner);
-				double answersCount = reasoner.countQueryAnswers(RuleParser.parsePositiveLiteral(queryString)).getCount();
-				System.out.println("  " + queryString + ": " + answersCount);
+			/* Get facts for all helper predicates */
+			for (int i=0; i<ruleIdx; i++) {
+				PositiveLiteral helperLiteral = helperLiterals.get(i);
+				System.out.println("Answers to query " + helperLiteral + " :");
+
+				try (final QueryResultIterator answers = reasoner.answerQuery(helperLiteral, true)) {
+					answers.forEachRemaining(answer -> System.out.println(" - " + answer));
+					System.out.println("Query answers are: " + answers.getCorrectness());
+				}
 			}
+
+			// /* Execute some queries */
+			// final List<String> queries = Arrays.asList("colour(?X)", "coloured(?X, ?C)", "vertex(?X)");
+			// System.out.println("\nNumber of inferred tuples for selected query atoms:");
+			// for (final String queryString : queries) {
+			// 	ExamplesUtils.printOutQueryAnswers(RuleParser.parsePositiveLiteral(queryString), reasoner);
+			// 	double answersCount = reasoner.countQueryAnswers(RuleParser.parsePositiveLiteral(queryString)).getCount();
+			// 	System.out.println("  " + queryString + ": " + answersCount);
+			// }
 		}
+
+		// Get grounded rules
 	}
 
 }
